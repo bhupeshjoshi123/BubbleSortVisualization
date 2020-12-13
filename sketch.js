5bd2cef1e6859b9a7ba6849b6135a6fda83f5ee6
bhupe <bhupeshjoshi293@gmail.com>
2020-12-13 10:23:33 +0545

    commit

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+
+let values = [];
+
+let i = 0;
+let j = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  values = new Array(1000);
+  for (let i = 0; i < values.length; i++) {
+	values[i] = random(500);
+	
+	console.log("values" + values[i]);
+	
+  }
+}
+
+function draw() {
+  
+	background(0);
+
+  if (i < values.length) {
+    for (let j = 0; j < values.length - i - 1; j++) {
+      let a = values[j];
+      let b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+	  }
+    }
+  } else {
+    console.log("done");
+    noLoop();
+  }
+  i++;
+
+  for (let i = 0; i < values.length; i++) {
+	
+	stroke(255, 204, 0);
+
+    line(i, height, i, height - values[i]);
+  }
+}
+
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
